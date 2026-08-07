7c4e516433806358a20eb6c32ad096b4d4c33d83
Krishnamoorthi-15 <krishna1776543@gmail.com>
2026-08-07 09:53:21 +0530

    if elseif else

---
+++ if_elseif_else.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+class adult{
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int age =sc.nextInt();
+        if(age>=18){
+            System.out.println("adult");
+        }
+        else if(age>=60){
+            
+        }
+        else{
+              System.out.println("minor");
+        }
+    }
+}
